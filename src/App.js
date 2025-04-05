f9ad9e9b50dd3d3d1cd2058d1292fcf185b3898f
victor <victorliu5000@gmail.com>
2025-04-05 10:09:53 -0400

    first push

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h className="text-4xl">HI</h>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#364156",
+        secondary: "#214E34",
+        button: "#CDCDCD",
+        font: "#DFF8EB",
+        accent: "#011638",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      fontWeight: {
+        light: 200,
+      },
+    },
+  },
+  plugins: [],
+};
